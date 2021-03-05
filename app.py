ae3acd531d2bf955405ae800c23ec983bbe29d0a
sreerajm104 <55447211+sreerajm104@users.noreply.github.com>
2021-03-05 14:13:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Created on Mon Feb 22 20:44:41 2021
 from flask import Flask, request, jsonify, render_template,Response
 import pandas as pd
 import numpy as np
+import requests
 
 
 from main import QGen
@@ -24,36 +25,46 @@ def generate():
     '''
     For rendering results on HTML GUI
     '''
-
-    # topic = request.form.values() #No error
-    topic = [x for x in request.form.values()]
+    try:
+        
+        # topic = request.form.values() #No error
+        topic = [x for x in request.form.values()]
+        
+        # return topic #remove post from decorater
+        qg = QGen()  
+        payload = {"input_text": topic[0]} 
+        
+        
+        output = qg.predict_shortq(payload)   
+        
+        return render_template('index.html', prediction_text=output)
     
-    # return topic #remove post from decorater
-    qg = QGen()  
-    payload = {"input_text": topic[0]} 
-    output = qg.predict_shortq(payload)   
-    
-    row_headers = list(list(output.values())[1].values())[0].keys()
-
-    return render_template('index.html', prediction_text=output)
-
+    except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error encountered in Generate: {e}")
+        return render_template('index.html', prediction_text=e)
+        
         
 @app.route('/generatefile',methods=['POST'])
 def generatefromfile():
     '''
     For rendering results on HTML GUI
     '''
+    try: 
+        file = request.files['file']
+        file.save(file.filename)    
+        with open(file.filename,'r') as f:    
+            str_text = f.read()
+        # str_text = "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in International cricket."
+        
+        qg = QGen()  
+        payload = {"input_text": str_text} 
+        output = qg.predict_shortq(payload)   
+        return output
+    except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error encountered in GenerateFile: {e}")
+        return "Error"
 
-    file = request.files['file']
-    file.save(file.filename)    
-    with open(file.filename,'r') as f:    
-        str_text = f.read()
-    # str_text = "Sachin Ramesh Tendulkar is a former international cricketer from India and a former captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in International cricket."
-    
-    qg = QGen()  
-    payload = {"input_text": str_text} 
-    output = qg.predict_shortq(payload)   
-    return output
+
 
 if __name__ == "__main__":
     app.run(debug=True)
