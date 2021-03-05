6c8f50a914bcbd055b00523c2c258200f433389f
sreerajm104 <55447211+sreerajm104@users.noreply.github.com>
2021-03-05 14:43:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -40,9 +40,8 @@ def generate():
         return render_template('index.html', prediction_text=output)
     
     except requests.exceptions.RequestException as e:
-        app.logger.error(f"Error encountered in Generate: {e}")
-        return render_template('index.html', prediction_text=e)
-        
+        app.logger.error(f"Error encountered in Generate: {e}")  
+        return render_template('503.html')
         
 @app.route('/generatefile',methods=['POST'])
 def generatefromfile():
@@ -62,9 +61,22 @@ def generatefromfile():
         return output
     except requests.exceptions.RequestException as e:
         app.logger.error(f"Error encountered in GenerateFile: {e}")
-        return "Error"
+        return render_template('503.html')
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
 
+@app.errorhandler(503)
+def page_not_found(error):
+    """Custom 503 page."""
+    return render_template('503.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
