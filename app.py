63d37a0b52e3f0e918f985ff39f09c19fa8105ba
sreerajm104 <55447211+sreerajm104@users.noreply.github.com>
2021-03-31 17:19:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,16 +9,104 @@ import pandas as pd
 import numpy as np
 import requests
 from werkzeug.exceptions import HTTPException
-
 import smtplib
 
-from main import QGen
+# from main import QGen
+
+import torch
+from transformers import T5ForConditionalGeneration,T5Tokenizer
+import spacy
+from sense2vec import Sense2Vec
+import nltk
+# import numpy 
+from nltk import FreqDist
+nltk.download('brown')
+nltk.download('stopwords')
+nltk.download('popular')
+from nltk.corpus import brown
+from similarity import levenshtein
+from mcq import tokenize_sentences
+from mcq import get_keywords
+from mcq import get_sentences_for_keyword
+from mcq import generate_normal_questions
+
+import time
 
 # New streamlit changes starts
 
 import streamlit as st
 
 def app():
+    
+# Class Starts
+    class QGen:
+        
+        def __init__(self):            
+            
+            self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
+            model = T5ForConditionalGeneration.from_pretrained('Parth/result')
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            model.to(device)
+            # model.eval()
+            self.device = device
+            self.model = model
+            self.nlp = spacy.load('en_core_web_sm')
+    
+            self.s2v = Sense2Vec().from_disk('s2v_old')
+    
+            self.fdist = FreqDist(brown.words())
+            self.normalized_levenshtein = levenshtein
+            self.set_seed(42)
+            
+        def set_seed(self,seed):
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed_all(seed)
+                
+        def predict_shortq(self, inp):
+            # inp = {
+            #     "input_text": payload.get("input_text"),
+            #     "max_questions": payload.get("max_questions", 15)
+            # }
+    
+            text = inp['input_text']
+            sentences = tokenize_sentences(text)
+            return sentences
+        def modified_text_gen(self,sentences):
+            
+            joiner = " "
+            modified_text = joiner.join(sentences)   
+            return modified_text
+            
+        def predict_sentences(self,sentences,modified_text):
+            
+            keywords = get_keywords(self.nlp,modified_text,inp['max_questions'],self.s2v,self.fdist,self.normalized_levenshtein,len(sentences) )
+            return keywords
+            
+        def final_predict(self,keywords,sentences,modified_text):
+            keyword_sentence_mapping = get_sentences_for_keyword(keywords, sentences)
+            
+            for k in keyword_sentence_mapping.keys():
+                text_snippet = " ".join(keyword_sentence_mapping[k][:3])
+                keyword_sentence_mapping[k] = text_snippet
+    
+            final_output = {}
+    
+            if len(keyword_sentence_mapping.keys()) == 0:
+                return final_output
+            else:
+                generated_questions = generate_normal_questions(keyword_sentence_mapping,self.device,self.tokenizer,self.model)
+            final_output["statement"] = modified_text
+            final_output["questions"] = generated_questions["questions"]
+            
+            if torch.device=='cuda':
+                torch.cuda.empty_cache()
+    
+            return final_output               
+
+#Class Ends    
+
     st.title("Frequently Asked Question Generator")
     
     st.write("1. Add your text corpus in the text area.")
@@ -30,14 +118,34 @@ def app():
     
     if st.button("Generate"):
         qg = QGen()  
-        payload = {"input_text": raw_text}         
-            
-        output = qg.predict_shortq(payload) 
+        payload = {"input_text": raw_text}   
+        inp = {
+            "input_text": payload.get("input_text"),
+            "max_questions": payload.get("max_questions", 15)
+            }
+        
+        sentences =qg.predict_shortq(inp)
+        st.write("Please wait while the sentence formation is processed")
+                
+        modified_text = qg.modified_text_gen(sentences)
+        st.success("Sentence formation and modification are completed. Please wait for mapping")
+        keywords = qg.predict_sentences(sentences,modified_text) 
+        
+        latest_iteration = st.empty()
+        bar = st.progress(0)
+        for i in range(100):  
+        # Update the progress bar with each iteration.
+            latest_iteration.text(f'Loading {i+1}')
+            bar.progress(i + 1)
+            time.sleep(0.1)
+        st.success("The sentence mapping is completed. Please wait for the table to be generated") 
+        # if st.button("Continue"):
+        output = qg.final_predict(keywords,sentences,modified_text)
         df = pd.DataFrame.from_dict(output['questions'])
         df = df.drop(labels=["id"],axis=1)        
         st.write(df)
-    
-    st.subheader(' ------------------------FAQ Generator---------------------- :sunglasses:')
+        
+    st.subheader(' -------------------------------------FAQ Generator--------------------------------- :sunglasses:')
 
 if __name__ == "__main__":
 	app()
